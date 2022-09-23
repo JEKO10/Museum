ad1a646d90a0d1c183e59d5994971142b80aa959
Aleksa Bubanja <aleksa3b@gmail.com>
2022-09-23 23:57:49 +0200

    576px breakpoint responsive for gallery and slider sections

--- main.js
+++ main.js
@@ -1,67 +1,67 @@
-// const menuBtn = document.querySelector(".menuBtn");
-// const menuBurger = document.querySelector(".menuBurger");
-// const navList = document.querySelector("nav ul");
+const menuBtn = document.querySelector(".menuBtn");
+const menuBurger = document.querySelector(".menuBurger");
+const navList = document.querySelector("nav ul");
 
-// menuBtn.addEventListener("click", () => {
-//   menuBurger.classList.toggle("open");
-//   navList.classList.toggle("hide");
+menuBtn.addEventListener("click", () => {
+  menuBurger.classList.toggle("open");
+  navList.classList.toggle("hide");
 
-//   if (menuBurger.classList.contains("open")) {
-//     document.body.style.overflow = "hidden";
-//   } else {
-//     document.body.style.overflow = "auto";
-//   }
-// });
+  if (menuBurger.classList.contains("open")) {
+    document.body.style.overflow = "hidden";
+  } else {
+    document.body.style.overflow = "auto";
+  }
+});
 
-// const left = document.querySelectorAll(".left");
-// const rightArrow = document.querySelector("#rightArrow");
-// const firstImg = document.querySelector("#firstImg");
-// const main = document.querySelector("#main");
-// const lastImg = document.querySelector("#lastImg");
-// const inner = document.querySelector(".inner");
-// let counter = 0;
-// let innerLeft;
-// const images = [
-//   "./images/gallery1.png",
-//   "./images/slider3.png",
-//   "./images/slider1.png",
-// ];
+const left = document.querySelectorAll(".left");
+const rightArrow = document.querySelector("#rightArrow");
+const firstImg = document.querySelector("#firstImg");
+const main = document.querySelector("#main");
+const lastImg = document.querySelector("#lastImg");
+const inner = document.querySelector(".inner");
+let counter = 0;
+let innerLeft;
+const images = [
+  "./images/gallery1.png",
+  "./images/slider3.png",
+  "./images/slider1.png",
+];
 
-// left.forEach((leftArrow) => {
-//   leftArrow.addEventListener("click", () => {
-//     counter--;
-//     lastImg.src = images[counter + 1];
-//     if (counter < 0) {
-//       counter = 2;
-//     }
-//     firstImg.src = images[counter - 1];
-//     if (counter === 0) {
-//       firstImg.src = images[2];
-//     }
-//     if (counter === 0) innerLeft = 50;
-//     if (counter === 1) innerLeft = 83;
-//     if (counter === 2) innerLeft = 18;
-//     inner.style.left = innerLeft + "%";
-//     main.src = images[counter];
-//   });
-// });
+left.forEach((leftArrow) => {
+  leftArrow.addEventListener("click", () => {
+    counter--;
+    lastImg.src = images[counter + 1];
+    if (counter < 0) {
+      counter = 2;
+    }
+    firstImg.src = images[counter - 1];
+    if (counter === 0) {
+      firstImg.src = images[2];
+    }
+    if (counter === 0) innerLeft = 50;
+    if (counter === 1) innerLeft = 83;
+    if (counter === 2) innerLeft = 18;
+    inner.style.left = innerLeft + "%";
+    main.src = images[counter];
+  });
+});
 
-// rightArrow.addEventListener("click", () => {
-//   counter++;
-//   firstImg.src = images[counter - 1];
-//   if (counter > 2) {
-//     counter = 0;
-//   }
-//   lastImg.src = images[counter + 1];
-//   if (counter === 2) {
-//     lastImg.src = images[0];
-//   }
-//   if (counter === 0) innerLeft = 50;
-//   if (counter === 1) innerLeft = 83;
-//   if (counter === 2) innerLeft = 18;
-//   inner.style.left = innerLeft + "%";
-//   main.src = images[counter];
-// });
+rightArrow.addEventListener("click", () => {
+  counter++;
+  firstImg.src = images[counter - 1];
+  if (counter > 2) {
+    counter = 0;
+  }
+  lastImg.src = images[counter + 1];
+  if (counter === 2) {
+    lastImg.src = images[0];
+  }
+  if (counter === 0) innerLeft = 50;
+  if (counter === 1) innerLeft = 83;
+  if (counter === 2) innerLeft = 18;
+  inner.style.left = innerLeft + "%";
+  main.src = images[counter];
+});
 
 const prev = document.querySelector("#prev");
 const next = document.querySelector("#next");
